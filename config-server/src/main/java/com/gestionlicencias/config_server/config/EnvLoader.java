a384468b8e5b502605d2a6602be42f3e8e4d9080
harold <harold1a96@gmail.com>
2025-06-06 00:30:34 -0500

    agregando los cambios para manejar contenedores

--- config-server/src/main/java/com/gestionlicencias/config_server/config/EnvLoader.java
+++ config-server/src/main/java/com/gestionlicencias/config_server/config/EnvLoader.java
@@ -4,8 +4,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("GIT_USERNAME", dotenv.get("GIT_USERNAME"));
-        System.setProperty("GIT_PASSWORD", dotenv.get("GIT_PASSWORD"));
+        //Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("GIT_USERNAME", System.getenv("GIT_USERNAME"));
+        System.setProperty("GIT_PASSWORD", System.getenv("GIT_PASSWORD"));
     }
 }
